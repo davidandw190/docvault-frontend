7dd9a9a1bd393db1413ce60a41e85e49fe157bdd
Andrei-David Nan <nandavid190@gmail.com>
2024-06-25 18:13:19 +0300

    v0.1.2 - setup login endpoint and ILoginCredentials;

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,24 +1,42 @@
-import { BASE_URL, isJsonContentType, processError, processResponse } from "../utils/request.utils";
+import {
+  BASE_URL,
+  isJsonContentType,
+  processError,
+  processResponse,
+} from '../utils/request.utils';
 
-import { IResponse } from "../models/IResponse";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
-import { User } from "../models/IUser";
+import { IResponse } from '../models/IResponse';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+import { User } from '../models/IUser';
+import { ILoginRequest } from '../models/ICredentails';
 
 export const userAPI = createApi({
-  reducerPath: "userAPI",
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL, credentials: "include", isJsonContentType  }),
-  tagTypes: ["User"],
+  reducerPath: 'userAPI',
+  baseQuery: fetchBaseQuery({
+    baseUrl: BASE_URL,
+    credentials: 'include',
+    isJsonContentType,
+  }),
+  tagTypes: ['User'],
   endpoints: (builder) => ({
     fetchUser: builder.query<IResponse<User>, void>({
       query: () => ({
-        url: "/profile",
-        method: "GET",
+        url: '/profile',
+        method: 'GET',
       }),
       keepUnusedDataFor: 120,
       transformResponse: processResponse<User>,
       transformErrorResponse: processError,
-      providesTags: (result, error) => ["User"],
-
-    })
+      providesTags: (result, error) => ['User'],
+    }),
+    loginUser: builder.mutation<IResponse<User>, ILoginRequest>({
+      query: (credentials) => ({
+        url: '/login',
+        method: 'POST',
+        body: credentials,
+      }),
+      transformResponse: processResponse<User>,
+      transformErrorResponse: processError,
+    }),
   }),
-})
+});
