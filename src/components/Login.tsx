408c4354f8a54e80c0d0d6f71dac5e30ac434319
Andrei-David Nan <nandavid190@gmail.com>
2024-06-29 21:54:01 +0300

    v0.1.8 -  introduce green highlight on valid login form input;

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useLocation } from 'react-router-dom';
+import { Link, Navigate, useLocation } from 'react-router-dom';
 
 import { ILoginRequest } from '../models/ICredentails';
 import { IResponse } from '../models/IResponse';
@@ -11,77 +11,146 @@ import { zodResolver } from '@hookform/resolvers/zod';
 
 const loginSchema = z.object({
   email: z.string().min(3, 'Email is required').email('Invalid email address'),
-  password: z.string().min(5, 'Password is required')
-})
+  password: z.string().min(5, 'Password is required'),
+});
 
-const Login = () => {
+const Login: React.FC = () => {
   const location = useLocation();
   const isLoggedIn: boolean =
     (JSON.parse(localStorage.getItem('')!) as boolean) || false;
   const [loginUser, { data, error, isLoading, isSuccess }] =
     userAPI.useLoginUserMutation();
 
-    const { register, handleSubmit, formState: form, getFieldState } = useForm<ILoginRequest>({
-      resolver: zodResolver(loginSchema),
-      mode: 'onTouched'
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: form,
+    getFieldState,
+  } = useForm<ILoginRequest>({
+    resolver: zodResolver(loginSchema),
+    mode: 'onTouched',
+  });
+
+  const isFieldValid = (fieldName: keyof ILoginRequest): boolean =>
+    getFieldState(fieldName, form).isTouched &&
+    !getFieldState(fieldName).invalid;
 
   const handleLogin = (credentials: ILoginRequest) => loginUser(credentials);
 
   if (isLoggedIn) {
-    return location?.state?.from?.pathname ? <Navigate to={location.state.from.pathname} replace/> : <Navigate to="/" replace/>;
+    return location?.state?.from?.pathname ? (
+      <Navigate to={location.state.from.pathname} replace />
+    ) : (
+      <Navigate to="/" replace />
+    );
   }
 
-  if (isSuccess && (!data?.data.user.mfa)) {
+  if (isSuccess && !data?.data.user.mfa) {
     localStorage.setItem('login', 'true');
-    return location?.state?.from?.pathname ? <Navigate to={location.state.from.pathname} replace/> : <Navigate to="/" replace/>;
+    return location?.state?.from?.pathname ? (
+      <Navigate to={location.state.from.pathname} replace />
+    ) : (
+      <Navigate to="/" replace />
+    );
   }
 
-
-  if (isSuccess &&  data?.data.user.mfa) {
+  if (isSuccess && data?.data.user.mfa) {
     // TODO: Return MFA component
-    return
+    return;
   }
 
   return (
     <div className="container">
       <div className="row justify-content-center">
-        <div className="col-lg-6 col-md-6 col-sm-12" style={{ marginTop: '150px' }}>
+        <div
+          className="col-lg-6 col-md-6 col-sm-12"
+          style={{ marginTop: '150px' }}
+        >
           <div className="card">
             <div className="card-body">
               <h4 className="mb-3">Login</h4>
-              {error && <div className="alert alert-dismissible alert-danger">
-                {'data' in error ? (error.data as IResponse<void>).message : 'An error occurred'}
-              </div>}
+              {error && (
+                <div className="alert alert-dismissible alert-danger">
+                  {'data' in error
+                    ? (error.data as IResponse<void>).message
+                    : 'An error occurred'}
+                </div>
+              )}
               <hr />
-              <form onSubmit={handleSubmit(handleLogin)} className="needs-validation" noValidate>
+              <form
+                onSubmit={handleSubmit(handleLogin)}
+                className="needs-validation"
+                noValidate
+              >
                 <div className="row g-3">
                   <div className="col-12">
-                    <label htmlFor="email" className="form-label">Email address</label>
+                    <label htmlFor="email" className="form-label">
+                      Email address
+                    </label>
                     <div className="input-group has-validation">
-                      <span className="input-group-text"><i className="bi bi-envelope"></i></span>
-                      <input type="text" {...register('email')} name='email' autoComplete="on"
-                        className={`form-control ' ${form.errors.email ? 'is-invalid' : ''}`}
-                        id="email" placeholder="Email address" disabled={false} required /> 
-                        {/* TODO: setup feedback when input is valid */}
-                      <div className="invalid-feedback">{form.errors.email?.message}</div>
+                      <span className="input-group-text">
+                        <i className="bi bi-envelope"></i>
+                      </span>
+                      <input
+                        type="text"
+                        {...register('email')}
+                        name="email"
+                        autoComplete="on"
+                        className={`form-control ' ${
+                          form.errors.email ? 'is-invalid' : ''
+                        } 
+                        ${isFieldValid('email') ? 'is-valid' : ''}`}
+                        id="email"
+                        placeholder="Email address"
+                        disabled={false}
+                        required
+                      />
+                      <div className="invalid-feedback">
+                        {form.errors.email?.message}
+                      </div>
                     </div>
                   </div>
                   <div className="col-12">
-                    <label htmlFor="password" className="form-label">Password</label>
+                    <label htmlFor="password" className="form-label">
+                      Password
+                    </label>
                     <div className="input-group has-validation">
-                      <span className="input-group-text"><i className="bi bi-key"></i></span>
-                      <input type="password" {...register('password')} name='password' autoComplete="on"
-                        className={`form-control ' ${form.errors.password ? 'is-invalid' : ''}`} placeholder="Password" disabled={false} required />
-                      {/* TODO: setup feedback when input is valid */}
-                      <div className="invalid-feedback">{form.errors.password?.message}</div>
+                      <span className="input-group-text">
+                        <i className="bi bi-key"></i>
+                      </span>
+                      <input
+                        type="password"
+                        {...register('password')}
+                        name="password"
+                        autoComplete="on"
+                        className={`form-control ' ${
+                          form.errors.password ? 'is-invalid' : ''
+                        } ${isFieldValid('password') ? 'is-valid' : ''}`}
+                        placeholder="Password"
+                        disabled={false}
+                        required
+                      />
+                      <div className="invalid-feedback">
+                        {form.errors.password?.message}
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div className="col mt-3">
-                  <button disabled={form.isSubmitting || isLoading} className="btn btn-primary btn-block" type="submit" >
-                    {(form.isSubmitting || isLoading) && <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>}
-                    <span role="status">{(form.isSubmitting || isLoading) ? 'Loading...' : 'Login'}</span>
+                  <button
+                    disabled={form.isSubmitting || isLoading}
+                    className="btn btn-primary btn-block"
+                    type="submit"
+                  >
+                    {(form.isSubmitting || isLoading) && (
+                      <span
+                        className="spinner-border spinner-border-sm"
+                        aria-hidden="true"
+                      ></span>
+                    )}
+                    <span role="status">
+                      {form.isSubmitting || isLoading ? 'Loading...' : 'Login'}
+                    </span>
                   </button>
                 </div>
               </form>
@@ -89,7 +158,9 @@ const Login = () => {
               <div className="row mb-3">
                 <div className="col d-flex justify-content-start">
                   <div className="btn btn-outline-light">
-                    <Link to="/register" style={{ textDecoration: 'none' }}>Create an Account</Link>
+                    <Link to="/register" style={{ textDecoration: 'none' }}>
+                      Create an Account
+                    </Link>
                   </div>
                 </div>
                 <div className="col d-flex justify-content-end">
@@ -103,7 +174,7 @@ const Login = () => {
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default Login;
