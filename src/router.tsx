6eceb19a6fa153ab0675be339401df0dac2e47b0
Andrei-David Nan <nandavid190@gmail.com>
2024-07-08 15:17:45 +0300

    v0.2.16 - secured needed routes and navbar component;

--- src/router.tsx
+++ src/router.tsx
@@ -10,6 +10,7 @@ import App from './App';
 import ForgottenPassword from './pages/ForgottenPassword';
 import Login from './pages/Login';
 import NavBar from './components/navigation/NavBar';
+import ProtectedRoute from './components/ProtectedRoute';
 import Registration from './pages/Registration';
 import ResetPassword from './pages/ResetPassword';
 
@@ -18,14 +19,16 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       {/* Public Routes */}
       <Route path="login" element={<Login />} />
-      <Route path='register' element={<Registration />} />
-      <Route path='verify/account' element={<AccountVerification />} />
-      <Route path='forgot-password' element={<ForgottenPassword />} />
-      <Route path='reset-password' element={<ResetPassword />} />
+      <Route path="register" element={<Registration />} />
+      <Route path="verify/account" element={<AccountVerification />} />
+      <Route path="forgot-password" element={<ForgottenPassword />} />
+      <Route path="reset-password" element={<ResetPassword />} />
       {/* Protected Routes */}
-      <Route element={<NavBar />}>
-        <Route index path='/documents' element={'documents'} />
-        <Route path='/' element={<Navigate to={'/documents'} />} />
+      <Route element={<ProtectedRoute />}>
+        <Route element={<NavBar />}>
+          <Route index path="/documents" element={'documents'} />
+          <Route path="/" element={<Navigate to={'/documents'} />} />
+        </Route>
       </Route>
     </Route>
   )
