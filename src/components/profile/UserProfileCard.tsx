40e25fa787cfc402fef39ef9005f2bc896e33bc4
Andrei-David Nan <nandavid190@gmail.com>
2024-07-08 18:02:12 +0300

    v0.3.1 - set up UserProfileCard component;

---
+++ src/components/profile/UserProfileCard.tsx
@@ -0,0 +1,55 @@
+import { IUser } from '../../models/IUser';
+
+type Props = {
+  user: IUser | undefined;
+  onSelectImage: () => void;
+  isLoading: boolean;
+};
+
+const UserProfileCard: React.FC<Props> = ({
+  user,
+  onSelectImage,
+  isLoading,
+}) => {
+  return (
+    <div className="card text-center mb-3">
+      <div className="card-body">
+        <img
+          src={user?.imageUrl}
+          className="img-fluid mx-auto user-photo"
+          alt={user?.firstName}
+        />
+        <a
+          onClick={onSelectImage}
+          className="btn btn-light border btn-sm card-text mb-2 opacity-80"
+          style={{ fontSize: '12px' }}
+        >
+          {!isLoading && (
+            <i className="bi bi-camera-fill" style={{ marginRight: '5px' }}></i>
+          )}
+          {isLoading && (
+            <span
+              className="spinner-border spinner-border-sm"
+              aria-hidden="true"
+            ></span>
+          )}
+          <span role="status">
+            {isLoading ? 'Changing...' : 'Change Photo'}
+          </span>
+        </a>
+        <p className="h6">
+          {user?.firstName} {user?.lastName}
+        </p>
+        <p className="card-text">
+          <i className="bi bi-shield-exclamation">
+            <span className="badge bg-primary-subtle text-primary-emphasis pill fst-normal">
+              {user?.role}
+            </span>
+          </i>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfileCard;
