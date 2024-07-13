96360b8afb62665f5350cfb05959eeeeee774c6b
Andrei-David Nan <nandavid190@gmail.com>
2024-07-13 23:14:46 +0300

    v0.4.0 -  setup IDocument and IPage interfaces and aferent types;

---
+++ src/models/IDocument.ts
@@ -0,0 +1,27 @@
+export interface IDocument {
+  id: number;
+  icon: string;
+  name: string;
+  description: string;
+  size: number;
+  extension: string;
+  uri: string;
+  formattedSize: string;
+  referenceId: string;
+  ownerLastLogin: string;
+  updaterName: string;
+  documentId: string;
+  createdAt: string;
+  updatedAt: string;
+  ownerName: string;
+  ownerEmail: string;
+  ownerPhone: string;
+}
+
+export type Document = { document: IDocument };
+
+export type Documents = { 'documents': IDocument[] };
+
+export type DocumentsQuery = { name?: string, page: number, size: number };
+
+export type DocumentForm = Pick<IDocument, 'name' | 'description' | 'documentId' | 'size' | 'formattedSize' | 'uri' | 'updaterName' | 'createdAt' | 'updatedAt'>;
